929f81c8403a86bbcc0cda1f724171bbae79e5e5
Capellán <jorlincap@gmail.com>
2017-04-23 16:26:48 -0400

    Finish

--- Bank/Controllers/PrestamosController.cs
+++ Bank/Controllers/PrestamosController.cs
@@ -65,13 +65,10 @@ namespace Bank.Controllers
             return View();
         }
 
-        // POST: Prestamos/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [Authorize(Roles = "admin")]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PrestamoID,Plazo,Monto,TasaInteres,TasaMora,Deuda,ProximoPago,Estado,GaranteID,ClienteID")] Prestamo prestamo)
+        public ActionResult Create([Bind(Include = "PrestamoID,Plazo,Monto,TasaInteres,TasaMora,Deuda,ProximoPago,Estado,ClienteID,Garante")] Prestamo prestamo)
         {
             if (ModelState.IsValid)
             {
